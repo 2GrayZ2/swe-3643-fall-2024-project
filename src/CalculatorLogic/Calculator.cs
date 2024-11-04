09e8143499e5957b49d23286acb625160957e1be
2GrayZ2 <145519917+2GrayZ2@users.noreply.github.com>
2024-11-04 17:53:33 -0500

    Wrote Compute Sample STD and Compute Population STD tests and logic
    Wrote unit tests and methods for ComputePopulationStandardDeviation and ComputeSampleStandardDeviation.

--- src/CalculatorLogic/Calculator.cs
+++ src/CalculatorLogic/Calculator.cs
@@ -17,6 +17,59 @@ namespace CalculatorLogic
             return sumAccumulator / valueList.Length;
         }
         
-        
+        public static double ComputeSquareOfDifferences(double [] valueList, double mean){
+            if(valueList == null || valueList.Length == 0){
+                throw new ArgumentException("valuesList parameter cannot be null or empty");
+            }
+            double squareAccumulator = 0;
+            for (int i = 0; i < valueList.Length; i++)
+            {
+                double difference = valueList[i] - mean;
+                double squareOfDifference = difference * difference;
+                squareAccumulator += squareOfDifference;
+            }
+            return squareAccumulator;
+            
+        }
+
+        public static double ComputeVariance(double squareOfDifferences, double numValues, bool isPopulation){
+            if(!isPopulation){
+                numValues -= 1;
+            }
+            if(numValues < 1){
+                throw new ArgumentException("numValues is too low (sample size must be >= 2, population size must be >= 1)");
+            }
+            return squareOfDifferences / numValues;
+        }
+
+        public static double ComputeStandardDeviation(double [] valueList, bool isPopulation){
+            if(valueList == null || valueList.Length == 0){
+                throw new ArgumentException("valuesList parameter cannot be null or empty");
+            }
+            double mean = ComputeMean(valueList);
+            double squareOfDifferences = ComputeSquareOfDifferences(valueList,mean);
+            double variance = ComputeVariance(squareOfDifferences, valueList.Length, isPopulation);
+
+            return Math.Sqrt(variance);
+        }
+
+        public static double ComputeSampleStandardDeviation(double [] valueList){
+            if(valueList == null || valueList.Length == 0){
+                throw new ArgumentException("valuesList parameter cannot be null or empty");
+            }
+            return ComputeStandardDeviation(valueList,false);
+        }
+
+        public static double ComputePopulationStandardDeviation(double [] valueList){
+            if(valueList == null || valueList.Length == 0){
+                throw new ArgumentException("valuesList parameter cannot be null or empty");
+            }
+            if (valueList.Length == 1)
+            {
+                throw new ArgumentException("valuesList parameter cannot contain one value");
+            }
+
+            return ComputeStandardDeviation(valueList, true);
+        }
     }
 }
--- src/CalculatorLogicUnitTests/CalculatorLogicUnitTests.cs
+++ src/CalculatorLogicUnitTests/CalculatorLogicUnitTests.cs
@@ -12,14 +12,14 @@ namespace CalculatorLogicUnitTests
         {
             //preq-UNIT-TEST-4
             // Arrange
-            double [] values = {1, 2, 3, 4, 5};
+            double[] values = { 1, 2, 3, 4, 5 };
 
             // Act
             double mean = Calculator.ComputeMean(values);
 
             // Assert
-            Assert.AreEqual(3.0, mean);  // Expected mean is 3.0
-            
+            Assert.AreEqual(3.0, mean); // Expected mean is 3.0
+
         }
 
         [Test]
@@ -27,13 +27,117 @@ namespace CalculatorLogicUnitTests
         {
             //preq-UNIT-TEST-4
             //Arrange
-            double [] values = {};
-            
+            double[] values = { };
+
             //Act
             Exception exception = Assert.Throws<ArgumentException>(() => Calculator.ComputeMean(values));
-            
+
             //Assert
             Assert.AreEqual(exception.Message, "valuesList parameter cannot be null or empty");
         }
+
+        [Test]
+        public void ComputeSampleStandardDeviation_ValidInput_ReturnsCorrectSampleStandardDeviation()
+        {
+            //preq-UNIT-TEST-2
+
+            //Arrange
+            double[] values = { 9, 6, 8, 5, 7 };
+
+            //Act
+            double sampleStdDev = Calculator.ComputeSampleStandardDeviation(values);
+
+            //Assert
+            Assert.AreEqual(1.5811388300841898, sampleStdDev);
+        }
+
+        [Test]
+        public void ComputeSampleStandardDeviation_EmptyArray_ThrowsArgumentException()
+        {
+            //preq-UNIT-TEST-2
+
+            //Arrange
+            double[] values = { };
+
+            //Act
+            Exception exception =
+                Assert.Throws<ArgumentException>(() => Calculator.ComputeSampleStandardDeviation(values));
+
+            //Assert
+            Assert.AreEqual(exception.Message, "valuesList parameter cannot be null or empty");
+        }
+
+        [Test]
+        public void ComputeSampleStandardDeviation_ArrayOfSameValues_ReturnsZero()
+        {
+            //preq-UNIT-TEST-2
+
+            //Arrange
+            double[] values = { 12, 12, 12, 12, 12 };
+
+            //Act
+            double sampleStdDev = Calculator.ComputeSampleStandardDeviation(values);
+
+            //Assert
+            Assert.AreEqual(0.0, sampleStdDev);
+        }
+
+        [Test]
+        public void ComputePopulationStandardDeviation_ArrayOfSameValues_ReturnsZero()
+        {
+            //preq-UNIT-TEST-2
+
+            //Arrange
+            double[] values = { 12, 12, 12, 12, 12 };
+
+            //Act
+            double sampleStdDev = Calculator.ComputePopulationStandardDeviation(values);
+
+            //Assert
+            Assert.AreEqual(0.0, sampleStdDev);
+        }
+
+        [Test]
+        public void ComputePopulationStandardDeviation_ValidInput_ReturnsCorrectPopulationStdDev()
+        {
+            // Arrange
+            double[] values = { 9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4 };
+
+            // Act
+            double popStdDev = Calculator.ComputePopulationStandardDeviation(values);
+
+            // Assert
+            Assert.AreEqual(2.9833, popStdDev, 0.0001); // Expected population std deviation
+        }
+
+        [Test]
+        public void ComputePopulationStandardDeviation_EmptyArray_ThrowsArgumentException()
+        {
+            // Arrange
+            double[] values = { };
+
+            //Act
+            Exception exception =
+                Assert.Throws<ArgumentException>(() => Calculator.ComputePopulationStandardDeviation(values));
+
+            //Assert
+            Assert.AreEqual(exception.Message, "valuesList parameter cannot be null or empty");
+        }
+
+        [Test]
+        public void ComputePopulationStandardDeviation_ArrayContainsOneValue_ReturnsZero()
+        {
+            // Arrange
+            double[] values = {1};
+
+            //Act
+            Exception exception =
+                Assert.Throws<ArgumentException>(() => Calculator.ComputePopulationStandardDeviation(values));
+
+            //Assert
+            Assert.AreEqual(exception.Message, "valuesList parameter cannot contain one value");
+        }
+
+
     }
 }
